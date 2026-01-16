a0fa9724438fb9f84bb97f3e1e0aefb6497138ad
rpatil <rpatil@sevenmentor.services>
2026-01-16 19:22:44 +0530

    romanToInt

---
+++ String/romanToInteger.js
@@ -0,0 +1,28 @@
+
+var romanToInt = function (s) {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let output = 0;
+  for (let i = 0; i < s.length; i++) {
+    let firstPointer = romanMap[s[i]];
+    if (i + 1 < s.length) {
+      let secondPointer = romanMap[s[i + 1]];
+      if (firstPointer < secondPointer) {
+        output += secondPointer - firstPointer;
+        i++;
+      } else {
+        output += firstPointer;
+      }
+    } else {
+      output += firstPointer;
+    }
+  }
+  return output;
+};
