03afbed7cba93803cc89f578fb338d137b20809c
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-01-27 13:52:41 +0530

    problem no 121 and 345 solved

---
+++ array/121_bestTimeToBuyAndSellStock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for(let i = 1; i < prices.length; i++){
+        minPrice = Math.min(prices[i], minPrice);
+
+        let profit = prices[i] - minPrice;
+
+        maxProfit = Math.max(profit, maxProfit);
+    }
+    return maxProfit;
+};
