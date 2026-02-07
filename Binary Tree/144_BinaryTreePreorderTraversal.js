ab3d58d87ea3955a38c495a9920005d77c407af2
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-02-07 14:48:12 +0530

    solved problem no 144

---
+++ Binary Tree/144_BinaryTreePreorderTraversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+    let result = [];
+    function checkNull(node) {
+        if (node == null) return;
+        result.push(node.val);
+        checkNull(node.left);
+        checkNull(node.right);
+    }
+    checkNull(root)
+    return result;
+};
