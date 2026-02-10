b23a57de4f7a2ef4fc00fd61e3cbaf0ec54615a5
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-02-10 13:55:04 +0530

    solved problem no 94

---
+++ Binary Tree/94_BinaryTreeInorderTraversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    let result = [];
+    function iot(node) {
+        if (node === null) return;
+        iot(node.left);
+        result.push(node.val);
+        iot(node.right);
+    }
+    iot(root);
+    return result;
+};
