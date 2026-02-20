424eef7272281d34f2e694b8e06115b51cbde926
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-02-20 15:56:13 +0530

    350. Intersection of Two Arrays II

---
+++ array/350_IntersectionOfTwoArraysII.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+
+    let map = {};
+    let result = [];
+
+    // count frequency of nums1
+    for (let num of nums1) {
+        map[num] = (map[num] || 0) + 1;
+    }
+
+    // check in nums2
+    for (let num of nums2) {
+        if (map[num] > 0) {
+            result.push(num);
+            map[num]--;
+        }
+    }
+
+    return result;
+};
